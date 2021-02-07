80ba48a175aebba9a7eb72951dfdfe784a720ccb
AnimeshRy <ani16september@gmail.com>
2021-02-07 16:38:05 +0530

    CRUD agents app done

--- agents/urls.py
+++ agents/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import AgentListView, AgentCreateView
+from .views import AgentDetailView, AgentListView, AgentCreateView, AgentUpdateView, AgentDeleteView
 
 app_name = 'agents'
 
 urlpatterns = [
     path('', AgentListView.as_view(), name='agent_list'),
+    path('<int:pk>/', AgentDetailView.as_view(), name="agent_detail"),
+    path('<int:pk>/update/', AgentUpdateView.as_view(), name="agent_update"),
+    path('<int:pk>/delete/', AgentDeleteView.as_view(), name="agent_delete"),
     path('create/', AgentCreateView.as_view(), name='agent_create')
+
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,5 +1,5 @@
 from django.contrib import auth
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
 from django.urls import reverse
@@ -25,3 +25,33 @@ class AgentCreateView(LoginRequiredMixin, CreateView):
         agent.organization = self.request.user.userprofile
         agent.save()
         return super().form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+
+class AgentUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+    def get_success_url(self):
+        return reverse("agents:agent_list")
+
+
+class AgentDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+    def get_success_url(self) -> str:
+        return reverse("agents:agent_list")
